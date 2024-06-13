b663a0627e360e81aeedbe9ba9f1dbf5874bd3d1
Suryanarayana8099 <162612193+Suryanarayana8099@users.noreply.github.com>
2024-06-13 19:16:13 +0530

    Update HomeController.java

--- src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
+++ src/main/java/com/application/covidtrackerapplication/controllers/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
 
-    @GetMapping("/")
+    @GetMapping("/covid")
     public String home(Model model){
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
